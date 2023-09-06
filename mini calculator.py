d7cd1dc1ebd582afa0b97e7b28f267667d1362c4
YUKESKAANTH V <109220333+yukes21@users.noreply.github.com>
2023-09-06 11:44:30 +0530

    Add files via upload

---
+++ mini calculator.py
@@ -0,0 +1,13 @@
+print("Mini Calculator")
+a=int(input("enter  A:"))
+b=int(input("enter B:"))
+operation=input("add/sub/mul/div:")
+if(operation=="add"):
+    print(a + b)
+elif (operation == "sub" ):
+      print( a - b )
+elif (operation == "mul"):
+     print(a * b)
+elif (operation == "div"):
+     print(a / b)     
+
